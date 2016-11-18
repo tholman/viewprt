d198cd293cc4a861f41994b05175e436c7bb3008
Garth Poitras <garth22@gmail.com>
2016-11-18 11:16:32 -0800

    0.3.0

--- dist/viewprt.js
+++ dist/viewprt.js
@@ -2,210 +2,207 @@
   typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
   typeof define === 'function' && define.amd ? define(['exports'], factory) :
   (factory((global.Viewprt = global.Viewprt || {})));
-}(this, function (exports) { 'use strict';
+}(this, (function (exports) { 'use strict';
 
-  var babelHelpers = {};
+var classCallCheck = function (instance, Constructor) {
+  if (!(instance instanceof Constructor)) {
+    throw new TypeError("Cannot call a class as a function");
+  }
+};
 
-  babelHelpers.classCallCheck = function (instance, Constructor) {
-    if (!(instance instanceof Constructor)) {
-      throw new TypeError("Cannot call a class as a function");
+var inherits = function (subClass, superClass) {
+  if (typeof superClass !== "function" && superClass !== null) {
+    throw new TypeError("Super expression must either be null or a function, not " + typeof superClass);
+  }
+
+  subClass.prototype = Object.create(superClass && superClass.prototype, {
+    constructor: {
+      value: subClass,
+      enumerable: false,
+      writable: true,
+      configurable: true
     }
-  };
-
-  babelHelpers.inherits = function (subClass, superClass) {
-    if (typeof superClass !== "function" && superClass !== null) {
-      throw new TypeError("Super expression must either be null or a function, not " + typeof superClass);
-    }
-
-    subClass.prototype = Object.create(superClass && superClass.prototype, {
-      constructor: {
-        value: subClass,
-        enumerable: false,
-        writable: true,
-        configurable: true
-      }
-    });
-    if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass;
-  };
-
-  babelHelpers.possibleConstructorReturn = function (self, call) {
-    if (!self) {
-      throw new ReferenceError("this hasn't been initialised - super() hasn't been called");
-    }
-
-    return call && (typeof call === "object" || typeof call === "function") ? call : self;
-  };
-
-  babelHelpers;
-
-  var win = typeof window !== 'undefined' && window;
-  var rAF = win && (win.requestAnimationFrame || function (callback) {
-    setTimeout(callback, 1000 / 60);
   });
+  if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass;
+};
 
-  function throttle(context, method) {
-    var isProcessing = false;
-    return function () {
-      if (!isProcessing) {
-        rAF(function () {
-          method.call(context);
-          isProcessing = false;
-        });
-        isProcessing = true;
-      }
-    };
+var possibleConstructorReturn = function (self, call) {
+  if (!self) {
+    throw new ReferenceError("this hasn't been initialised - super() hasn't been called");
   }
 
-  function toggleEventListeners(handler, container, add) {
-    if (win) {
-      var method = add ? 'addEventListener' : 'removeEventListener';
-      win[method]('resize', handler);
-      container[method]('scroll', handler);
+  return call && (typeof call === "object" || typeof call === "function") ? call : self;
+};
+
+var win = typeof window !== 'undefined' && window;
+var rAF = win && (win.requestAnimationFrame || function (callback) {
+  setTimeout(callback, 1000 / 60);
+});
+
+function throttle(context, method) {
+  var isProcessing = false;
+  return function () {
+    if (!isProcessing) {
+      rAF(function () {
+        method.call(context);
+        isProcessing = false;
+      });
+      isProcessing = true;
     }
+  };
+}
+
+function toggleEventListeners(handler, container, add) {
+  if (win) {
+    var method = add ? 'addEventListener' : 'removeEventListener';
+    win[method]('resize', handler);
+    container[method]('scroll', handler);
+  }
+}
+
+var ViewportObserver = function () {
+  function ViewportObserver() {
+    var opts = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
+    classCallCheck(this, ViewportObserver);
+
+    this.container = opts.container || win;
+    this.offset = ~~opts.offset || 0;
+    this.isObserving = false;
+    this.throttledProcess = throttle(this, this.process);
   }
 
-  var ViewportObserver = function () {
-    function ViewportObserver() {
-      var opts = arguments.length <= 0 || arguments[0] === undefined ? {} : arguments[0];
-      babelHelpers.classCallCheck(this, ViewportObserver);
+  ViewportObserver.prototype.start = function start() {
+    if (!this.isObserving) {
+      toggleEventListeners(this.throttledProcess, this.container, true);
+      this.isObserving = true;
+    }
+    return this;
+  };
 
-      this.container = opts.container || win;
-      this.offset = ~ ~opts.offset || 0;
+  ViewportObserver.prototype.stop = function stop() {
+    if (this.isObserving) {
+      toggleEventListeners(this.throttledProcess, this.container);
       this.isObserving = false;
-      this.throttledProcess = throttle(this, this.process);
     }
+  };
 
-    ViewportObserver.prototype.start = function start() {
-      if (!this.isObserving) {
-        toggleEventListeners(this.throttledProcess, this.container, true);
-        this.isObserving = true;
-      }
-      return this;
-    };
+  ViewportObserver.prototype.process = function process() {/* Logic here */};
 
-    ViewportObserver.prototype.stop = function stop() {
-      if (this.isObserving) {
-        toggleEventListeners(this.throttledProcess, this.container);
-        this.isObserving = false;
-      }
-    };
+  return ViewportObserver;
+}();
 
-    ViewportObserver.prototype.process = function process() {/* Logic here */};
+function isElementInViewport(element) {
+  var offset = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 0;
+  var viewportWidth = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : window.innerWidth;
+  var viewportHeight = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : window.innerHeight;
 
-    return ViewportObserver;
-  }();
+  var rect = element.getBoundingClientRect();
+  return !!(rect.width && rect.height) && rect.bottom > 0 - offset && rect.right > 0 - offset && rect.left < viewportWidth + offset && rect.top < viewportHeight + offset;
+}
 
-  function isElementInViewport(element) {
-    var offset = arguments.length <= 1 || arguments[1] === undefined ? 0 : arguments[1];
-    var viewportWidth = arguments.length <= 2 || arguments[2] === undefined ? window.innerWidth : arguments[2];
-    var viewportHeight = arguments.length <= 3 || arguments[3] === undefined ? window.innerHeight : arguments[3];
+var ViewportElementObserver = function (_ViewportObserver) {
+  inherits(ViewportElementObserver, _ViewportObserver);
 
-    if (element) {
-      var rect = element.getBoundingClientRect();
-      return !!(rect.width && rect.height) && rect.bottom > 0 - offset && rect.right > 0 - offset && rect.left < viewportWidth + offset && rect.top < viewportHeight + offset;
-    }
-    return false;
+  function ViewportElementObserver() {
+    var opts = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
+    classCallCheck(this, ViewportElementObserver);
+
+    var _this = possibleConstructorReturn(this, _ViewportObserver.call(this, opts));
+
+    _this.onEnteredViewport = opts.onEnteredViewport;
+    _this._guid = 0;
+    _this._queue = {};
+    return _this;
   }
 
-  var ViewportElementObserver = function (_ViewportObserver) {
-    babelHelpers.inherits(ViewportElementObserver, _ViewportObserver);
-
-    function ViewportElementObserver() {
-      var opts = arguments.length <= 0 || arguments[0] === undefined ? {} : arguments[0];
-      babelHelpers.classCallCheck(this, ViewportElementObserver);
-
-      var _this = babelHelpers.possibleConstructorReturn(this, _ViewportObserver.call(this, opts));
-
-      _this.onEnteredViewport = opts.onEnteredViewport;
-      _this._guid = 0;
-      _this._queue = {};
-      return _this;
+  ViewportElementObserver.prototype.add = function add(item) {
+    var element = item && item.element || item;
+    if (!(element instanceof Element)) {
+      throw new Error('Add an `Element` or object with an `element` property');
     }
 
-    ViewportElementObserver.prototype.add = function add(item) {
-      var element = item && item.element || item;
-      if (!(element instanceof Element)) {
-        throw new Error('Add an `Element` or object with an `element` property');
-      }
+    if (isElementInViewport(element, this.offset)) {
+      this.onEnteredViewport(item);
+    } else {
+      var id = '' + this._guid++;
+      this._queue[id] = item;
+      this.start();
+      return id;
+    }
+  };
 
-      if (isElementInViewport(element, this.offset)) {
-        this.onEnteredViewport(item);
-      } else {
-        var id = '' + this._guid++;
-        this._queue[id] = item;
-        this.start();
-        return id;
+  ViewportElementObserver.prototype.removeById = function removeById(id) {
+    var queue = this._queue;
+    if (queue[id]) {
+      delete queue[id];
+      if (! --this._guid) {
+        this.stop();
       }
-    };
+    }
+  };
 
-    ViewportElementObserver.prototype.removeById = function removeById(id) {
-      var queue = this._queue;
-      if (queue[id]) {
-        delete queue[id];
-        if (! --this._guid) {
-          this.stop();
+  ViewportElementObserver.prototype.process = function process() {
+    var queue = this._queue;
+    var viewportWidth = window.innerWidth;
+    var viewportHeight = window.innerHeight;
+    var id = void 0,
+        item = void 0,
+        element = void 0;
+
+    for (id in queue) {
+      if (queue.hasOwnProperty(id)) {
+        item = queue[id];
+        element = item.element || item;
+        if (!element || !element.parentNode) {
+          this.removeById(id);
+        } else if (isElementInViewport(element, this.offset, viewportWidth, viewportHeight)) {
+          this.onEnteredViewport(item);
+          this.removeById(id);
         }
       }
-    };
+    }
+  };
 
-    ViewportElementObserver.prototype.process = function process() {
-      var queue = this._queue;
-      var viewportWidth = window.innerWidth;
-      var viewportHeight = window.innerHeight;
-      var id = void 0,
-          item = void 0,
-          element = void 0;
+  return ViewportElementObserver;
+}(ViewportObserver);
 
-      for (id in queue) {
-        if (queue.hasOwnProperty(id)) {
-          item = queue[id];
-          element = item.element || item;
-          if (isElementInViewport(element, this.offset, viewportWidth, viewportHeight)) {
-            this.onEnteredViewport(item);
-            this.removeById(id);
-          }
-        }
-      }
-    };
+var ViewportPositionObserver = function (_ViewportObserver) {
+  inherits(ViewportPositionObserver, _ViewportObserver);
 
-    return ViewportElementObserver;
-  }(ViewportObserver);
+  function ViewportPositionObserver() {
+    var opts = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
+    classCallCheck(this, ViewportPositionObserver);
 
-  var ViewportPositionObserver = function (_ViewportObserver) {
-    babelHelpers.inherits(ViewportPositionObserver, _ViewportObserver);
+    var _this = possibleConstructorReturn(this, _ViewportObserver.call(this, opts));
 
-    function ViewportPositionObserver() {
-      var opts = arguments.length <= 0 || arguments[0] === undefined ? {} : arguments[0];
-      babelHelpers.classCallCheck(this, ViewportPositionObserver);
+    _this.onReachedBottom = opts.onReachedBottom;
+    _this.onReachedTop = opts.onReachedTop;
+    return _this;
+  }
 
-      var _this = babelHelpers.possibleConstructorReturn(this, _ViewportObserver.call(this, opts));
+  ViewportPositionObserver.prototype.process = function process() {
+    var container = this.container;
+    var isWindow = container === window;
+    var element = isWindow ? document.body : container;
+    var elementHeight = isWindow ? window.innerHeight : element.offsetHeight;
+    var yOffset = isWindow ? window.pageYOffset : element.scrollTop;
 
-      _this.onReachedBottom = opts.onReachedBottom;
-      _this.onReachedTop = opts.onReachedTop;
-      return _this;
+    if (this.onReachedBottom && elementHeight + yOffset + this.offset >= element.scrollHeight) {
+      this.onReachedBottom(element);
     }
 
-    ViewportPositionObserver.prototype.process = function process() {
-      var container = this.container;
-      var isWindow = container === window;
-      var element = isWindow ? document.body : container;
-      var elementHeight = isWindow ? window.innerHeight : element.offsetHeight;
-      var yOffset = isWindow ? window.pageYOffset : element.scrollTop;
+    if (this.onReachedTop && yOffset === 0) {
+      this.onReachedTop(element);
+    }
+  };
 
-      if (this.onReachedBottom && elementHeight + yOffset + this.offset >= element.scrollHeight) {
-        this.onReachedBottom(element);
-      }
+  return ViewportPositionObserver;
+}(ViewportObserver);
 
-      if (this.onReachedTop && yOffset === 0) {
-        this.onReachedTop(element);
-      }
-    };
+exports.ViewportObserver = ViewportObserver;
+exports.ViewportElementObserver = ViewportElementObserver;
+exports.ViewportPositionObserver = ViewportPositionObserver;
 
-    return ViewportPositionObserver;
-  }(ViewportObserver);
+Object.defineProperty(exports, '__esModule', { value: true });
 
-  exports.ViewportObserver = ViewportObserver;
-  exports.ViewportElementObserver = ViewportElementObserver;
-  exports.ViewportPositionObserver = ViewportPositionObserver;
-
-}));
+})));
